c0a5bc07cfbc0317e793120150eb484a0edefffb
freshmre <chemrouk.ous@gmail.com>
2021-07-24 18:18:07 +0100

    Added UI to rps game

--- game-logic.js
+++ game-logic.js
@@ -6,19 +6,20 @@ function computerPlay() {
 
 function playRound(playerSelection, computerSelection) {
     /* Decides winner of a single game 
+       expected input 'Rock', 'Paper' or 'Scissors'
        returnedArray[0] = 1: Win for player, 0: Tie, -1: Loss
        returnedArray[1] = formatted outcome message
        playerSelection is case insensitive */
 
     playerSelection = playerSelection.slice(0, 1).toUpperCase() + playerSelection.slice(1).toLowerCase();
 
-    if (playerSelection === computerSelection){
+    if (playerSelection === computerSelection) {
         return [0, `It's a tie, both played ${computerSelection}`]
     }
 
     else if ((playerSelection === 'Rock' && computerSelection === 'Scissors') ||
-            (playerSelection === 'Paper' && computerSelection === 'Rock') ||
-            (playerSelection === 'Scissors' && computerSelection === 'Paper')) {
+        (playerSelection === 'Paper' && computerSelection === 'Rock') ||
+        (playerSelection === 'Scissors' && computerSelection === 'Paper')) {
         return [1, `You Win! ${playerSelection} beats ${computerSelection}`]
     }
 
@@ -27,26 +28,37 @@ function playRound(playerSelection, computerSelection) {
     }
 }
 
-function game() {
-    /* Game of 5 rounds
-       Shows score at the end */
-    
-    let gameOutcome;
-    let playerScore = 0;
-    let computerScore = 0;
-    for (let gamesLeft = 5; gamesLeft > 0; gamesLeft--) {
-        gameOutcome = playRound(prompt('Rock, Paper or Scissors? '), computerPlay());
-        switch (gameOutcome[0]) {
+let wins = ties = losses = 0;
+
+const choiceBtns = document.querySelectorAll("button");
+const announcement = document.querySelector('#top-text');
+const results = document.querySelector('#results');
+
+
+choiceBtns.forEach((btn) => {
+    btn.addEventListener('click', () => {
+        let output;
+        let games = 5;
+        let computerChoice = computerPlay();
+        let gameoutcome = playRound(btn.id, computerChoice);
+        announcement.innerHTML = gameoutcome[1];
+        switch (gameoutcome[0]) {
             case 1:
-                playerScore += 1;
+                wins += 1;
+                break;
+            case 0:
+                ties += 1;
                 break;
             case -1:
-                computerScore += 1;
+                losses += 1;
                 break;
         }
-        alert(gameOutcome[1]);
-    }
-    alert(`Match Ended\nYou: ${playerScore} Computer: ${computerScore}`);
-}
-
-game();
+        if (wins >= games || losses >= games) {
+            if (wins >= games) { output = `You Won the game! ${wins} - ${losses}`; }
+            else if (losses >= games) { output = `You Lost the game ! ${wins} - ${losses}`; }
+            wins = ties = losses = 0;
+        }
+        else output = `You: ${wins} | Ties: ${ties} | Computer: ${losses}`;
+        results.innerHTML = output;
+    })
+})
